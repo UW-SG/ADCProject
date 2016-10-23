19f26f14d86919326714f01cec8c58127d4299c5
Mohit Nayak <Mohit Nayak>
2016-10-22 18:51:28 -0700

    Logging changes

--- src/com/uw/adc/rmi/util/Constants.java
+++ src/com/uw/adc/rmi/util/Constants.java
@@ -5,5 +5,5 @@ import org.apache.log4j.Logger;
 public class Constants {
 
     public static final Logger UDP_STATS_LOGGER = Logger.getLogger("UDP_CLIENT_STATISTICS_LOG");
-    public static final Logger LOGGER = Logger.getLogger("udpClientLog");
+    public static final Logger LOGGER = Logger.getLogger("udpLog");
 }
